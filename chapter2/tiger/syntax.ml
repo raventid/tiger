05753d99695bdd86c83ece39fc780c80db1d179f
Julian Pokrovsky <juliankul@gmail.com>
2024-09-23 17:44:06 +0800

    add token type to tiger lexer

---
+++ chapter2/tiger/syntax.ml
@@ -0,0 +1,45 @@
+type token =
+    | TYPE
+    | VAR
+    | FUNCTION
+    | BREAK
+    | OF
+    | END
+    | IN
+    | NIL
+    | LET
+    | DO
+    | TO
+    | FOR
+    | WHILE
+    | ELSE
+    | THEN
+    | IF
+    | ARRAY
+    | ASSIGN
+    | OR
+    | AND
+    | GE
+    | GT
+    | LE
+    | LT
+    | NEQ
+    | EQ
+    | DIVIDE
+    | TIMES
+    | MINUS
+    | PLUS
+    | DOT
+    | RBRACE
+    | LBRACE
+    | RBRACK
+    | LBRACK
+    | RPAREN
+    | LPAREN
+    | SEMICOLON
+    | COLON
+    | COMMA
+    | STRING of string
+    | INT of int
+    | ID of string
+    | EOF
