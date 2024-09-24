9050527178138cbe1980aa8d9146189efed778f7
Julian Pokrovsky <juliankul@gmail.com>
2024-09-24 10:39:18 +0800

    implement lexer for a Tiger language

--- chapter2/tiger/driver.ml
+++ chapter2/tiger/driver.ml
@@ -1,3 +1,22 @@
 open Core
 
-let () = print_endline "placeholder"
+let rec tokens lexbuf =
+  match Lexer.read lexbuf with
+  | EOF -> [Syntax.EOF]
+  | token -> token::tokens lexbuf
+
+let lex_print ch =
+  ch
+  |> Lexing.from_channel
+  |> tokens
+  |> List.iter ~f:(fun token -> Format.printf "%s\n" (Syntax.show token))
+
+let run filename () =
+  In_channel.with_file filename ~f:lex_print
+
+let () =
+  Command.basic_spec
+    ~summary:"Parse and display JSON"
+    Command.Spec.(empty +> anon ("filename" %: string))
+    run
+  |> Command_unix.run
