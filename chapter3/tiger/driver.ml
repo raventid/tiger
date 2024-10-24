501d8ab64dfa3dc731c47c35aad97f59899cab96
Julian Pokrovsky <juliankul@gmail.com>
2024-10-24 16:34:27 +0800

    ch3: formatter

--- chapter3/tiger/driver.ml
+++ chapter3/tiger/driver.ml
@@ -15,13 +15,13 @@ let create_token_stream filename : (unit -> Parser.token * Lexing.position * Lex
   let next_token () =
     match In_channel.input_line ic with
     | None ->
-        In_channel.close ic;
-        (Parser.EOF, Lexing.dummy_pos, Lexing.dummy_pos)
+      In_channel.close ic;
+      (Parser.EOF, Lexing.dummy_pos, Lexing.dummy_pos)
     | Some line ->
-        let token = parse line in
-        printf "%s |  token: %s, line: %d \n" filename (show token) !line_no;
-        incr line_no;
-        (Syntax.to_parser token, Lexing.dummy_pos, Lexing.dummy_pos)
+      let token = parse line in
+      printf "%s |  token: %s, line: %d \n" filename (show token) !line_no;
+      incr line_no;
+      (Syntax.to_parser token, Lexing.dummy_pos, Lexing.dummy_pos)
   in
   next_token
 
@@ -35,20 +35,20 @@ let with_test_files directory f =
 let directory = "./shared/lexed/"
 
 let main () =
-let failed_to_parse_files = ref [] in
-with_test_files
-  directory
-  (fun filename ->
-     try
-       create_token_stream filename |> revised_parser
-     with
-       _ ->
+  let failed_to_parse_files = ref [] in
+  with_test_files
+    directory
+    (fun filename ->
+       try
+         create_token_stream filename |> revised_parser
+       with
+         _ ->
          printf "Fatal: Cannot build AST from %s token stream \n" filename;
          failed_to_parse_files := filename :: !failed_to_parse_files
-  );
+    );
 
-printf "\n\nFinished the execution of a test suite!\n";
-printf "Failed to parse next program samples: \n";
-List.iter !failed_to_parse_files (fun f -> printf "%s\n" f)
+  printf "\n\nFinished the execution of a test suite!\n";
+  printf "Failed to parse next program samples: \n";
+  List.iter !failed_to_parse_files (fun f -> printf "%s\n" f)
 
 let _ = main ()
